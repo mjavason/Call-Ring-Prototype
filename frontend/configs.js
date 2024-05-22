baff39c04119553730ea49e83998717fb72dbc1f
Orji Michael Chukwuebuka <orjimichael@4886@gmail.com>
2024-05-22 19:14:17 +0100

    Activate live signalling server

--- frontend/configs.js
+++ frontend/configs.js
@@ -1,5 +1,5 @@
-const socket = io('http://localhost:3000');
-// const socket = io('https://call-ring-prototype-backend.onrender.com');
+// const socket = io('http://localhost:3000');
+const socket = io('https://call-ring-prototype-backend.onrender.com');
 
 const userIdInput = document.getElementById('userId');
 const calleeIdInput = document.getElementById('calleeId');
